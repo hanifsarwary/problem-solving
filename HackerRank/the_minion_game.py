005bc6055f23a8f8302da34e6d83003ae4c79d18
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-01-16 11:39:11 +0500

    Hacker rank python challange of the minion game

---
+++ HackerRank/the_minion_game.py
@@ -0,0 +1,57 @@
+"""
+PROBLEM_STATEMENT
+
+Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string.
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string.
+
+For Example:
+String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+
+Input Format
+
+A single line of input containing the string .
+Note: The string  will contain only uppercase letters: .
+
+Constraints
+
+
+
+Output Format
+
+Print one line: the name of the winner and their score separated by a space.
+
+If the game is a draw, print Draw.
+"""
+
+def make_substring(string, index):
+    return len(string) - index
+
+def minion_game(string):
+    stuart_count = kevin_count = 0
+    for i in range(len(string)):
+        if string[i] in ('A', 'E', 'I','O', 'U'):
+            kevin_count += make_substring(string, i)
+        else:
+            stuart_count += make_substring(string, i)
+    if stuart_count > kevin_count:
+        print("Stuart", stuart_count)
+    elif stuart_count == kevin_count:
+        print("Draw")
+    else:
+        print("Kevin", kevin_count)
+    
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
--- Misc/README.md
+++
@@ -1,4 +0,0 @@
-problem1: This problem was asked by Microsoft.Given an unsorted array of integers, find the length of the longest consecutive elements sequence. given [100, 4, 200, 1, 3, 2], the longest consecutive element sequence is [1, 2, 3, 4]. Return its length: 4.Your algorithm should run in O(n) complexity.
-
-Problem2: This problem was asked by Google. Given a list of numbers and a number k, return whether any two numbers from the list add up to k.For example, given [10, 15, 3, 7] and k of 17, return true
-
